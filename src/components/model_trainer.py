4090d6ad0e8d6af1e3f1a72afef1792671b110ce
Harddik Singh <harddik05@gmail.com>
2024-10-27 20:26:07 +0530

    Hyperparameter Training is done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -39,17 +39,62 @@ class ModelTrainer:
 
             )
             models={
-                "Random Forest": RandomForestRegressor(),
-                "Decision Tree":DecisionTreeRegressor(),
-                "Gradient Boosting":GradientBoostingRegressor(),
-                "Linear Regression":LinearRegression(),
-                "K-Neighbors Classifier":KNeighborsRegressor(),
-                "XGBCLassifier":XGBRegressor(),
-                "CatBoosting Classifier": CatBoostRegressor(verbose=False),
-                "AdaBoost Classifier": AdaBoostRegressor(),
+                "RandomForestRegressor": RandomForestRegressor(),
+                "DecisionTreeRegressor":DecisionTreeRegressor(),
+                "GradientBoostingRegressor":GradientBoostingRegressor(),
+                "LinearRegression":LinearRegression(),
+                "KNeighborsRegressor":KNeighborsRegressor(),
+                "XGBRegressor":XGBRegressor(),
+                "CatBoostingRegressor": CatBoostRegressor(verbose=False),
+                "AdaBoostRegressor": AdaBoostRegressor(),
             }
 
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            params={
+                "DecisionTreeRegressor":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson']
+                    #'max_depth':[5,10,15],
+                    #'criterion':['gini', 'entropy']
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "RandomForestRegressor":{
+                    # 'loss':['sqaured_error','friedman_mse','absolute_error','poissn'],
+
+                    #'max_features':['sqrt','log2',None],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "GradientBoostingRegressor":{
+                    # 'loss':['squared_error','huber','absolute_error','quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error','friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "LinearRegression":{},
+                "KNeighborsRegressor":{
+                    'n_neighbors':[5,7,9,11],
+                    #'weights':['uniform','distance'],
+                    #'algorithm':['ball_tree','kd_tree','brute']
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "CatBoostingRegressor":{
+                    "depth":[6,8,10],
+                    "learning_rate":[0.01,0.05,0.1],
+                    "iterations":[30,50,100]
+                },
+                "AdaBoostRegressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
+            }
+
+
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
 
             ## To get best model score from dict
             best_model_score=max(sorted(model_report.values()))
@@ -76,7 +121,7 @@ class ModelTrainer:
         
         except Exception as e:
             raise CustomException(e,sys)
-        
+       
 
 
             
